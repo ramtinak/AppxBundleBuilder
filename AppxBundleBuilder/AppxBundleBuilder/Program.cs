fc1e2e0434594cbc7f2aaba3bf3a90c5b59efc40
ramtin <ramtinak@live.com>
2021-07-13 12:45:23 +0430

    Add FindFolders

--- AppxBundleBuilder/AppxBundleBuilder/Program.cs
+++ AppxBundleBuilder/AppxBundleBuilder/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿/*
+ * License: MIT
+ * Created by Ramtin Jokar [ Ramtinak@live.com ]
+ * 
+ * July 13, 2021
+ * 
+ */
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using static System.Console;
 
 namespace AppxBundleBuilder
 {
     class Program
     {
+        static readonly Stopwatch StopwatchAll = new();
+
         static void Main()
         {
 
+        }
+
+
+        static string[] FindFolders(string folder, string search)
+        {
+            return Directory.GetDirectories(folder, search, SearchOption.AllDirectories)
+                .ToArray();
         }
     }
 }
