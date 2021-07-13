445210017e35483da016a54ed7ce2609e5b260fd
ramtin <ramtinak@live.com>
2021-07-13 12:43:18 +0430

    Add Platform classes

---
+++ AppxBundleBuilder/AppxBundleBuilder/Platforms.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * License: MIT
+ * Created by Ramtin Jokar [ Ramtinak@live.com ]
+ * 
+ * July 13, 2021
+ * 
+ */
+
+using System.Collections.Generic;
+
+namespace AppxBundleBuilder
+{
+    public class ConfigurationSettings
+    {
+        public string SolutionDir { get; set; }
+        public string ProjectDir { get; set; }
+        public bool RestoreNugetPackages { get; set; }
+        public bool GenerateAppxUpload { get; set; }
+        public bool DeleteBinObjFolders { get; set; }
+        public List<Platform> Platforms { get; set; } = new List<Platform>();
+    }
+
+    public class Platform
+    {
+        public string AppxBundlePlatforms { get; set; }
+        public string MinimumSdkVersion { get; set; }
+        public string AppVersion { get; set; }
+        public List<Dependency> Dependencies { get; set; } = new List<Dependency>();
+    }
+
+    public class Dependency
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+    }
+}
